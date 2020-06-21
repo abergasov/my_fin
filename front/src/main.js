3f29a16bf679eddfe23792c2074291512e2ddcdb
Alexander Bergasov <a.bergasov@city-mobil.ru>
2020-06-21 14:28:15 +0300

    vuetify framework add

--- front/src/main.js
+++ front/src/main.js
@@ -1,24 +1,32 @@
-// Import Vue
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+// =========================================================
+// * Vuetify Material Dashboard - v2.1.0
+// =========================================================
+//
+// * Product Page: https://www.creative-tim.com/product/vuetify-material-dashboard
+// * Copyright 2019 Creative Tim (https://www.creative-tim.com)
+//
+// * Coded by Creative Tim
+//
+// =========================================================
+//
+// * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-// Import Vue App, routes, store
-import App from './App';
-import routes from './routes';
-import vuetify from '@/plugins/vuetify' // path to vuetify export
-Vue.use(VueRouter);
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './plugins/base'
+import './plugins/chartist'
+import './plugins/vee-validate'
+import vuetify from './plugins/vuetify'
+import i18n from './i18n'
 
-
-// Configure router
-const router = new VueRouter({
-    routes,
-    linkActiveClass: 'active',
-    mode: 'history'
-});
+Vue.config.productionTip = false
 
 new Vue({
-    vuetify,
-    el: '#app',
-    render: h => h(App),
-    router
-});
+  router,
+  store,
+  vuetify,
+  i18n,
+  render: h => h(App),
+}).$mount('#app')
