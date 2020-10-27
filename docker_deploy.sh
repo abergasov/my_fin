46c0ef15631e0dca842e1c92d1c00c434317e95f
Alexander Bergasov <a.bergasov@city-mobil.ru>
2020-10-27 15:44:37 +0300

    docler script add

---
+++ docker_deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+clean() {
+  echo "stop containers";
+  docker container stop app_main.mf webserver.mf dbMysql.mf
+  echo "drop containers"
+  docker rm -v app_main.mf webserver.mf dbMysql.mf
+}
+
+clean
+
+echo "run prod config"
+echo "RUN docker-compose.yml "
+docker-compose -f docker-compose.yml pull
+docker-compose -f docker-compose.yml up --build
